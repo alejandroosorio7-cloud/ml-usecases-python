d68b2b7cdd0d097f3c8df84843a45badaf2eddb2
alejandroosorio7-cloud <alejandro.osorio7@udea.edu.co>
2026-05-28 11:13:17 -0500

    Update answer-0001.py

--- myanswers/answer-0001.py
+++ myanswers/answer-0001.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+def detectar_olas_calor(df, umbral):
+    df = df.copy()
+
+    df['fecha'] = pd.to_datetime(df['fecha'])
+
+    df['dia_semana'] = df['fecha'].dt.day_name()
+
+    df = df[df['temperatura'] > umbral]
+
+    return df
