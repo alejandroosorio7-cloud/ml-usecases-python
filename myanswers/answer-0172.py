7d09b895f14a38026d0557fe3e548cfcb38490d9
alejandroosorio7-cloud <alejandro.osorio7@udea.edu.co>
2026-05-28 11:32:19 -0500

    Create answer-0172.py

---
+++ myanswers/answer-0172.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.cluster import KMeans
+
+
+def agrupar_pacientes(df, n_clusters):
+
+    kmeans = KMeans(
+        n_clusters=n_clusters,
+        n_init=10
+    )
+
+    labels = kmeans.fit_predict(df)
+
+    return labels
